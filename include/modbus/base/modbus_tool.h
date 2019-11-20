ef7b11488c625528c1ca2df081fb6e96a81bc35e
paopaol <467847281@qq.com>
2019-11-20 20:42:51 +0800

    move appendCrc to modbus::tool namespace

--- include/modbus/base/modbus_tool.h
+++ include/modbus/base/modbus_tool.h
@@ -17,6 +17,19 @@ public:
   }
 
   static uint16_t crc16_modbus(const uint8_t *data, size_t size);
+  /**
+   * Calculate the crc check of data and then return data+crc.
+The crc check is added in the order of the first low order and the high order.
+   */
+  static inline ByteArray appendCrc(const ByteArray &data) {
+    uint16_t crc = crc16_modbus((uint8_t *)(data.data()), data.size());
+    auto dataWithCrc = data;
+    /// first push low bit
+    dataWithCrc.push_back(crc % 256);
+    /// second push high bit
+    dataWithCrc.push_back(crc / 256);
+    return dataWithCrc;
+  }
 };
 
 } // namespace modbus
