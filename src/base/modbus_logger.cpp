e385623ddcb3472a04493ec20e31a759184b5ca9
paopaol <467847281@qq.com>
2019-12-04 00:05:32 +0800

    add default logger
    Signed-off-by: paopaol <467847281@qq.com>

--- src/base/modbus_logger.cpp
+++ src/base/modbus_logger.cpp
@@ -2,13 +2,21 @@
 #include <modbus/base/modbus.h>
 
 namespace modbus {
-static LogWriter g_logger;
+static LogWriter g_logger = [](LogLevel level, const std::string &msg) {
+  switch (level) {
+  case LogLevel::kDebug:
+    std::cout << "[Debug] " << msg << std::endl;
+    break;
+  case LogLevel::kInfo:
+    std::cout << "[Info] " << msg << std::endl;
+    break;
+  case LogLevel::kWarning:
+    std::cout << "[Warning] " << msg << std::endl;
+    break;
+  }
+};
 void registerLogMessage(const LogWriter &logger) { g_logger = logger; }
 
-void log(LogLevel level, const std::string &msg) {
-  if (g_logger) {
-    g_logger(level, msg);
-  }
-}
+void log(LogLevel level, const std::string &msg) { g_logger(level, msg); }
 
 } // namespace modbus
