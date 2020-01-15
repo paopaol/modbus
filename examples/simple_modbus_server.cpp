cd2b170554ac9693a763e30c6901daa88c88e965
jinzhao <467847281@qq.com>
2020-01-15 19:16:42 +0800

    add tcp server
    Signed-off-by: jinzhao <467847281@qq.com>

---
+++ examples/simple_modbus_server.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <modbus/tools/modbus_server.h>
+
+int main(int argc, char *argv[]) {
+  QCoreApplication app(argc, argv);
+
+  auto modbusServer = modbus::createQModbusTcpServer(33333);
+  modbusServer->setServerAddress(0x01);
+
+  modbus::SingleBitAccess access;
+  access.setStartAddress(0x00);
+  access.setQuantity(0x10);
+  access.setValue(0x00, modbus::BitValue::kOn);
+  access.setValue(0x01, modbus::BitValue::kOff);
+  access.setValue(0x02, modbus::BitValue::kOn);
+  access.setValue(0x03, modbus::BitValue::kOff);
+  access.setValue(0x04, modbus::BitValue::kOn);
+
+  modbusServer->handleFunc(modbus::FunctionCode::kReadCoils, access);
+
+  modbusServer->listenAndServe();
+
+  return app.exec();
+}
