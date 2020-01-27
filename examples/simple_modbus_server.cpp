73f46487c3029ec824c7c9ad0e265c94ecd88ddc
jinzhao <467847281@qq.com>
2020-01-27 20:28:19 +0800

    update example modbus server
    Signed-off-by: jinzhao <467847281@qq.com>

--- examples/simple_modbus_server.cpp
+++ examples/simple_modbus_server.cpp
@@ -17,9 +17,26 @@ int main(int argc, char *argv[]) {
   access->setValue(0x03, modbus::BitValue::kOff);
   access->setValue(0x04, modbus::BitValue::kOn);
 
+  std::shared_ptr<modbus::SixteenBitAccess> sixteenAccess(
+      new modbus::SixteenBitAccess);
+  sixteenAccess->setStartAddress(0x00);
+  sixteenAccess->setQuantity(0x20);
+  sixteenAccess->setValue(0x00, modbus::SixteenBitValue(0x00, 0x11).toUint16());
+  sixteenAccess->setValue(0x03, modbus::SixteenBitValue(0x22, 0x33).toUint16());
+
   modbusServer->handleFunc(modbus::FunctionCode::kReadCoils, access);
+  modbusServer->handleFunc(modbus::FunctionCode::kReadInputDiscrete, access);
   modbusServer->handleFunc(modbus::FunctionCode::kWriteSingleCoil, access);
   modbusServer->handleFunc(modbus::FunctionCode::kWriteMultipleCoils, access);
+
+  modbusServer->handleFunc(modbus::FunctionCode::kReadInputRegister,
+                           sixteenAccess);
+  modbusServer->handleFunc(modbus::FunctionCode::kReadHoldingRegisters,
+                           sixteenAccess);
+  modbusServer->handleFunc(modbus::FunctionCode::kWriteSingleRegister,
+                           sixteenAccess);
+  modbusServer->handleFunc(modbus::FunctionCode::kWriteMultipleRegisters,
+                           sixteenAccess);
 
   bool success = modbusServer->listenAndServe();
   if (!success) {
