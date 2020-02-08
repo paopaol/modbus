fb3cac004dba1da852c3ea2c4985d9493ba993a4
jinzhao <467847281@qq.com>
2020-02-08 15:56:24 +0800

    read singlebits finished signal add function code args
    Signed-off-by: jinzhao <467847281@qq.com>

--- include/modbus/tools/modbus_client.h
+++ include/modbus/tools/modbus_client.h
@@ -179,7 +179,8 @@ signals:
   void clientClosed();
   void errorOccur(const QString &errorString);
   void requestFinished(const Request &request, const Response &response);
-  void readSingleBitsFinished(ServerAddress serverAddress, Address startAddress,
+  void readSingleBitsFinished(ServerAddress serverAddress,
+                              FunctionCode functionCode, Address startAddress,
                               const QVector<BitValue> &valueList, Error error);
   void writeSingleCoilFinished(ServerAddress serverAddress, Address address,
                                Error error);
