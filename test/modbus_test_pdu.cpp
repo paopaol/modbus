a81c6442f012dea20ef15ab93f0479459caab453
paopaol <467847281@qq.com>
2020-05-03 15:40:54 +0800

    Optimize unit test readability

--- test/modbus_test_pdu.cpp
+++ test/modbus_test_pdu.cpp
@@ -1,35 +1,45 @@
 #include "modbus_test_mocker.h"
 
+using namespace modbus;
+
 TEST(TestPdu, modbusPduConstructor) {
   auto dataChecker = MockReadCoilsDataChecker::newDataChecker();
-  modbus::Pdu pdu0(modbus::FunctionCode::kReadCoils, dataChecker);
-  auto fcode0 = pdu0.functionCode();
-  EXPECT_EQ(fcode0, modbus::FunctionCode::kReadCoils);
+  Pdu pdu(FunctionCode::kReadCoils, dataChecker);
+  auto fcode = pdu.functionCode();
+  EXPECT_EQ(fcode, FunctionCode::kReadCoils);
 }
 
 TEST(TestPdu, modbusPduConstructor_default_isInvalidFunctionCode) {
-  modbus::Pdu pdu0;
-  auto fcode0 = pdu0.functionCode();
-  EXPECT_EQ(fcode0, modbus::FunctionCode::kInvalidCode);
+  Pdu pdu;
+  auto fcode = pdu.functionCode();
+  EXPECT_EQ(fcode, FunctionCode::kInvalidCode);
 }
 
 TEST(TestPdu, modbusPduFunctionCodeTest) {
-  modbus::Pdu pdu1;
-  pdu1.setFunctionCode(modbus::FunctionCode::kReadCoils);
-  auto fcode = pdu1.functionCode();
-  EXPECT_EQ(fcode, modbus::FunctionCode::kReadCoils);
+  Pdu pdu;
+  pdu.setFunctionCode(FunctionCode::kReadCoils);
+  auto fcode = pdu.functionCode();
+  EXPECT_EQ(fcode, FunctionCode::kReadCoils);
 }
 
 TEST(TestPdu, modbusPduExpecptionTest) {
-  modbus::Pdu pdu2;
-  pdu2.setFunctionCode(modbus::FunctionCode(modbus::FunctionCode::kReadCoils +
-                                            modbus::Pdu::kExceptionByte));
-  EXPECT_EQ(true, pdu2.isException());
-  auto fcode = pdu2.functionCode();
-  EXPECT_EQ(fcode, modbus::FunctionCode::kReadCoils);
+  Pdu pdu;
+  pdu.setFunctionCode(
+      FunctionCode(FunctionCode::kReadCoils + Pdu::kExceptionByte));
+
+  struct Result {
+    bool isException;
+    FunctionCode fcode;
+  } expect{true, FunctionCode::kReadCoils};
+
+  Result actual;
+  actual.isException = pdu.isException();
+  actual.fcode = pdu.functionCode();
+
+  EXPECT_EQ(actual, expect);
 }
 
 TEST(TestPdu, modbusPdu_defaultFunctionCode_isInvalid) {
-  modbus::Pdu pdu4;
-  EXPECT_EQ(modbus::FunctionCode::kInvalidCode, pdu4.functionCode());
+  Pdu pdu;
+  EXPECT_EQ(FunctionCode::kInvalidCode, pdu.functionCode());
 }
