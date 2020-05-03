728ce38e69107c0997ab5f1ec15c117ccf15b239
paopaol <467847281@qq.com>
2020-05-03 17:19:12 +0800

    Optimize unit test readability

--- test/modbus_test_adu.cpp
+++ test/modbus_test_adu.cpp
@@ -1,26 +1,35 @@
 
 #include "modbus_test_mocker.h"
 
+using namespace modbus;
+
 TEST(TestModbusAdu, modbusAduConstructor) {
   auto dataChecker = MockReadCoilsDataChecker::newDataChecker();
-  modbus::Adu adu0(modbus::ServerAddress(1), modbus::FunctionCode::kReadCoils,
-                   dataChecker);
-  EXPECT_EQ(modbus::FunctionCode::kReadCoils, adu0.functionCode());
+  Adu adu(ServerAddress(1), FunctionCode::kReadCoils, dataChecker);
+  EXPECT_EQ(FunctionCode::kReadCoils, adu.functionCode());
+}
 
-  modbus::Adu adu1(modbus::ServerAddress(1),
-                   modbus::Pdu(modbus::FunctionCode::kReadCoils, dataChecker));
-  EXPECT_EQ(modbus::FunctionCode::kReadCoils, adu1.functionCode());
+TEST(TestModbusAdu, modbusAduConstructor2) {
+  auto dataChecker = MockReadCoilsDataChecker::newDataChecker();
+  Adu adu(ServerAddress(1), Pdu(FunctionCode::kReadCoils, dataChecker));
+  EXPECT_EQ(FunctionCode::kReadCoils, adu.functionCode());
 }
 
 TEST(TestAdu, modbusAduMarshalData) {
-  modbus::Adu adu;
+  struct Result {
+    int size;
+    modbus::ByteArray data;
+  } expect{5, ByteArray({0x01, 0x01, 0x01, 0x02, 0x03})};
+
+  Adu adu;
+  struct Result actual;
 
   adu.setServerAddress(0x1);
-  adu.setFunctionCode(modbus::FunctionCode::kReadCoils);
+  adu.setFunctionCode(FunctionCode::kReadCoils);
   adu.setData({1, 2, 3});
-  size_t buildSize = adu.marshalSize();
-  EXPECT_EQ(buildSize, 1 + 1 + 3 /*serveraddress|function code|data*/);
-  modbus::ByteArray buildData = adu.marshalAduWithoutCrc();
-  EXPECT_EQ(buildData,
-            modbus::ByteArray({1, modbus::FunctionCode::kReadCoils, 1, 2, 3}));
+  actual.size = adu.marshalSize();
+  actual.data = adu.marshalAduWithoutCrc();
+
+  EXPECT_EQ(actual.data, expect.data);
+  EXPECT_EQ(actual.size, expect.size);
 }
