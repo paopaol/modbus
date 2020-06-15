c957c0f57ceba0704e4019dcf7dcc031a22baf24
paopaol <467847281@qq.com>
2020-06-15 18:20:42 +0800

    thrd library use the same compiler with master project,if the building host has multiple compilers

--- thrd/fmt.cmake
+++ thrd/fmt.cmake
@@ -4,7 +4,9 @@ ExternalProject_Add(fmtlib
 		URL             ${CMAKE_CURRENT_SOURCE_DIR}/fmt-6.1.2.tar.gz
 		PREFIX fmtlib
 		CMAKE_ARGS
-						-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-						-DFMT_TEST=OFF
+		    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+		    -DFMT_TEST=OFF
+		    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+		    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
 )
 set_target_properties(fmtlib PROPERTIES FOLDER "fmtlib")
