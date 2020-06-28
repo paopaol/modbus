8e19c2555f61a1f5a222a5c5862e84dbc7fc55e0
paopaol <467847281@qq.com>
2020-06-28 14:25:45 +0800

    fix compile faield if windows installed multiple msvc versions

--- thrd/fmt.cmake
+++ thrd/fmt.cmake
@@ -4,6 +4,7 @@ ExternalProject_Add(fmtlib
 		URL             ${CMAKE_CURRENT_SOURCE_DIR}/fmt-6.1.2.tar.gz
 		PREFIX fmtlib
 		CMAKE_ARGS
+		    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 		    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 		    -DFMT_TEST=OFF
 		    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
