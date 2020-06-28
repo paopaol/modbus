e5e90553a3a2ceceb7632ea374adf20aa0ebb20f
paopaol <467847281@qq.com>
2020-06-28 14:25:45 +0800

    fix compile faield if windows installed multiple msvc versions

--- thrd/gtest.cmake
+++ thrd/gtest.cmake
@@ -4,6 +4,7 @@ ExternalProject_Add(googletest
 		URL             ${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.1.tar.gz
 		PREFIX googletest
 		CMAKE_ARGS
+		    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 		    -Dgtest_force_shared_crt=ON
 		    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 		    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
