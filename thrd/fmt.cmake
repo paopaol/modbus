de2c939b1a6c646c83dc6f023a9945a820f2be31
jinzhao <467847281@qq.com>
2020-12-12 23:02:14 +0800

    format cmake file,and fix compile failed under centos

--- thrd/fmt.cmake
+++ thrd/fmt.cmake
@@ -1,13 +1,12 @@
 include(ExternalProject)
 
-ExternalProject_Add(fmtlib
-		URL             ${CMAKE_CURRENT_SOURCE_DIR}/fmt-6.1.2.tar.gz
-		PREFIX fmtlib
-		CMAKE_ARGS
-		    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-		    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-		    -DFMT_TEST=OFF
-		    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-		    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-)
+ExternalProject_Add(
+  fmtlib
+  URL ${CMAKE_CURRENT_SOURCE_DIR}/fmt-6.1.2.tar.gz
+  PREFIX fmtlib
+  CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+             -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+             -DFMT_TEST=OFF
+             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER})
 set_target_properties(fmtlib PROPERTIES FOLDER "fmtlib")
--- thrd/gtest.cmake
+++ thrd/gtest.cmake
@@ -1,13 +1,12 @@
 include(ExternalProject)
 
-ExternalProject_Add(googletest
-		URL             ${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.1.tar.gz
-		PREFIX googletest
-		CMAKE_ARGS
-		    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-		    -Dgtest_force_shared_crt=ON
-		    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-		    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-		    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-)
+ExternalProject_Add(
+  googletest
+  URL ${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.1.tar.gz
+  PREFIX googletest
+  CMAKE_ARGS -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+             -Dgtest_force_shared_crt=ON
+             -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+             -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+             -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER})
 set_target_properties(googletest PROPERTIES FOLDER "googletest")
